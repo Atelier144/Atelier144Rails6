34c8f0a6a6781e89ff83c3497a0022d187ac81c6
AkioMabuchi <mabuchiakio@gmail.com>
2020-01-28 21:19:54 +0900

    SEO ready

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+    def default_meta_tags
+        {
+            site: "Atelier144",
+            title: "オリジナルフリーゲーム配信サイト",
+            reverse: true,
+            charset: "utf-8",
+            description: "Atelier144はたったひとりの個人によって開発運営されている会員登録制のフリーゲーム配信サイトです。",
+            keywords: ["フリーゲーム","ブラウザゲーム"],
+            icon:[
+                { href: image_url("favicon.ico")}
+            ]
+        }
+    end
 end
